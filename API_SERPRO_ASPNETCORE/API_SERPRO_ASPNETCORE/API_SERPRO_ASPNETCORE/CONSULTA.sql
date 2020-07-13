0aac991387fc527abf51f3fd0d2dd3e3e9f21add
Fernando Pantoja <antonioinformatic@gmail.com>
2020-07-13 10:13:23 -0300

    IMAGENS E CONSULTA

--- API_SERPRO_ASPNETCORE/API_SERPRO_ASPNETCORE/API_SERPRO_ASPNETCORE/CONSULTA.sql
+++ API_SERPRO_ASPNETCORE/API_SERPRO_ASPNETCORE/API_SERPRO_ASPNETCORE/CONSULTA.sql
@@ -1,7 +1,14 @@
 DECLARE @retono [varchar](MAX) = ''
-
-SELECT @retono = [dbo].[udf_SerproConsultaPF]()
-
+SELECT @retono = [dbo].[udf_SerproConsultaPF](00000000434) --CPF REGULAR/IRREGULAR
 print @retono
+select * FROM [dbo].[split](@retono,',')
 
+
+SELECT @retono = [dbo].[udf_SerproConsultaPF](8864778911) --CPF INVALIDO!
+print @retono
+select * FROM [dbo].[split](@retono,',')
+
+
+SELECT @retono = [dbo].[udf_SerproConsultaPF](61068664061) --CPF CPF NÃO ENCONTRADO !
+print @retono
 select * FROM [dbo].[split](@retono,',')
